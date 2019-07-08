fac2d7dc0060c02214642a100ac7ede60be93667
sunfengchuan <634580924@qq.com>
2019-07-08 09:45:54 +0800

    AppserviceTest;

---
+++ src/main/java/com/w8/sunfch/springbootweb/service/IApp2.java
@@ -0,0 +1,9 @@
+package com.w8.sunfch.springbootweb.service;
+
+import com.w8.sunfch.springbootweb.entity.StudentEntity;
+import org.springframework.validation.annotation.Validated;
+
+public interface IApp2 {
+
+
+}
