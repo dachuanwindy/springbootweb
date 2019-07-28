84ec45e66be62c880ea4e0290004918c2b9ad17c
sunfengchuan <sunfengchuan@58.com>
2019-07-28 15:44:47 +0800

    通过环境变量获取值 environment

--- src/main/java/com/w8/sunfch/springbootweb/util/ProInit.java
+++ src/main/java/com/w8/sunfch/springbootweb/util/ProInit.java
@@ -3,6 +3,7 @@ package com.w8.sunfch.springbootweb.util;
 import com.w8.sunfch.springbootweb.entity.SunEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -15,12 +16,19 @@ public class ProInit {
     @Autowired
     private SunEntity sunEntity;
 
+    @Autowired
+    private Environment environment;
+
     @PostConstruct
     public void test() {
 
         System.out.println("初始化要加载的内容++++++++++++");
         log.info("=============");
         System.out.println("输出的名字是" + sunEntity.getName());
+
+        /**可以通过环境变量获取值，Spring 将properties中的值变成了Spring的内部变量了*/
+        String property = environment.getProperty("book.sunfch");
+        System.out.println("通过环境变量获取值====" + property);
 
     }
 
