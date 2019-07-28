80e5b6af6152ebde6909caf0d88917c2346828e2
sunfengchuan <sunfengchuan@58.com>
2019-07-28 15:25:44 +0800

    添加配置文件 my.properties,然后能自己读取变量 2019年7月28日 15:25:30

--- src/main/java/com/w8/sunfch/springbootweb/entity/SunEntity.java
+++ src/main/java/com/w8/sunfch/springbootweb/entity/SunEntity.java
@@ -2,15 +2,20 @@ package com.w8.sunfch.springbootweb.entity;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @Data
+@PropertySource(value = "my.properties")
 public class SunEntity {
 
-    @Value("${sunfch}")
+    @Value("${book.sunfch}")
     private String name;
 
-    @Value("${classNum}")
+    @Value("${book.classNum}")
     private String classNo;
+
+    @Value("${book.ceshi}")
+    private String test;
 }
